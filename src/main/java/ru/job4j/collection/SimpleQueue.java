61a11b094f7ae7468967efb3c6df15397a2e49e6
mikle <mikle>
2024-01-21 12:45:08 +0300

    5. Очередь на двух стеках [#160 #475202]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -6,7 +6,6 @@ public class SimpleQueue<T> {
     private final SimpleStack<T> input = new SimpleStack<>();
     private final SimpleStack<T> output = new SimpleStack<>();
     private int size;
-    private boolean changeStack;
     /* добавьте переменные, если они требуются */
 
     public T poll() {
