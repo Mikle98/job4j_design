019dead535272b9d2f67bb3125ce4995fb2365e7
mikle <mikle>
2024-01-25 18:45:42 +0300

    3. Создать модель User [#999 #475917]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,12 +1,18 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
-    User() {
+    User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
     }
 }
