afdc2d0d4251e2f58061d6c793d4d5a58cf4a3e6
mikle <mikle>
2024-03-04 20:38:39 +0300

    11. Поиск дубликатов [#315066 #483640]

--- src/test/java/ru/job4j/io/duplicates/DuplicatesFinderTest.java
+++ src/test/java/ru/job4j/io/duplicates/DuplicatesFinderTest.java
@@ -2,9 +2,14 @@ package ru.job4j.io.duplicates;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -14,11 +19,13 @@ class DuplicatesFinderTest {
         DuplicateSearchAndValid duplSearch = new DuplicateSearchAndValid();
         Files.walkFileTree(Path.of("./JAVA"), duplSearch);
         String rsl = duplSearch.getDupl();
-        String newLine = System.lineSeparator();
-        Path path = Path.of("./JAVA").toAbsolutePath();
-        assertThat(rsl).isEqualTo("[" + path + "\\Text Document.txt - 4"
-                + newLine + ", " + path + "\\Новая папка\\Text Document.txt - 4"
-                + newLine + ", " + path + "\\Новая папка\\Новая папка\\Text Document.txt - 4"
-                + newLine + "]");
+        List<String> ex = new ArrayList<>();
+        Path path = Path.of("./JAVA/Text Document.txt");
+        ex.add(String.format("%s - %d%n", path.toAbsolutePath(), path.toFile().length()));
+        Path path2 = Path.of("./JAVA/Новая папка/Text Document.txt");
+        ex.add(String.format("%s - %d%n", path2.toAbsolutePath(), path2.toFile().length()));
+        Path path3 = Path.of("./JAVA/Новая папка/Новая папка/Text Document.txt");
+        ex.add(String.format("%s - %d%n", path3.toAbsolutePath(), path3.toFile().length()));
+        assertThat(rsl).isEqualTo(ex.toString());
     }
 }
