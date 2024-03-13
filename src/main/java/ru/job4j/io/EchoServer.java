a5a8f19523c0d8821f4642fed652f03067d1b252
mikle <mikle>
2024-03-13 22:07:28 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,20 +6,27 @@ import java.net.Socket;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
-        boolean isClosed = false;
         try (ServerSocket server = new ServerSocket(9000)) {
-            while (!isClosed) {
+            while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String string = input.readLine(); string != null && !string.isEmpty(); string = input.readLine()) {
-                        if ("GET".equals(string.split(" ")[0])
-                            && "Bye".equals(string.substring(string.indexOf("/?msg=") + 6, string.indexOf(" HTTP/1.1")))) {
-                            isClosed = true;
+                    String string = input.readLine();
+                    if (string != null && !string.isEmpty()) {
+                        if ("GET".equals(string.split(" ")[0])) {
+                            switch (string.substring(string.indexOf("/?msg=") + 6, string.indexOf(" HTTP/1.1"))) {
+                                case "Hello" -> {
+                                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                    output.write("Hello, dear friend.".getBytes());
+                                }
+                                case "Exit" -> server.close();
+                                default -> {
+                                    output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                                    output.write("What?".getBytes());
+                                }
+                            }
                         }
-                        System.out.println(string);
                     }
                     output.flush();
                 }
